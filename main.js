a8534b0155562b01c3852530efd6e94943c31781
i-guerrero <isaacguerrero@pursuit.org>
2022-08-24 19:53:26 -0400

    Implement Pixel Painter

--- main.js
+++ main.js
@@ -7,3 +7,36 @@ for (let i = 0; i < 100; i++) {
 }
 
 // You may write your code here!
+
+// Update height and width of each cell according to i
+
+// Add event listener for when palette is clicked
+const black = document.querySelector;
+
+// loop through class color and add Event listener
+// make current color match style
+
+const colorPalette = document.querySelectorAll(".color");
+
+const currentColor = document.querySelector("#current-color");
+
+for (let color of colorPalette) {
+  color.addEventListener("click", (event) => {
+    const backgroundColorDeclarationStr =
+      "background: " + event.target.style.backgroundColor;
+    currentColor.setAttribute("style", backgroundColorDeclarationStr);
+  });
+}
+
+// loop over grid
+
+const cells = document.querySelectorAll(".cell");
+// change background color of cell to be color of current color
+for (let cell of cells) {
+  cell.addEventListener("click", (event) => {
+    cell.setAttribute(
+      "style",
+      "background: " + currentColor.style.backgroundColor
+    );
+  });
+}
